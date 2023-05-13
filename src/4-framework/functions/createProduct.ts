c3c9758cd675b9d3f8238308a5a7442b85cead0d
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-13 14:38:59 -0300

    feat: add create product function

---
+++ src/4-framework/functions/createProduct.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata'
+import '../ioc/inversify.config'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+
+import { httpHandler } from '../utility/httpHandler'
+import { container } from '../shared/ioc/container'
+import { httpResponse } from '../utility/httpResponse'
+import { CreateProductOperator } from '../../3-controller/operators/createProductOperator'
+import { InputCreateProduct } from '../../3-controller/serializers/inputCreateProduct'
+
+export const handler = httpHandler(async (event: APIGatewayProxyEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  const operator = container.get(CreateProductOperator)
+  const body = JSON.parse(event?.body as string)
+
+  const input = new InputCreateProduct(body)
+  const result = await operator.exec(input)
+
+  if (result.isLeft()) {
+    return httpResponse.badRequest(result.value)
+  }
+
+  return httpResponse.created(result.value)
+})
