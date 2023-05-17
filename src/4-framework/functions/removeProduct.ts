4374d902474be9b36d68e6432e49491ac04395a0
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-17 16:00:04 -0300

    feat: add remove product lambda

---
+++ src/4-framework/functions/removeProduct.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata'
+import '../ioc/inversify.config'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+
+import { httpHandler } from '../utility/httpHandler'
+import { container } from '../shared/ioc/container'
+import { httpResponse } from '../utility/httpResponse'
+import { InputRemoveProduct } from '../../3-controller/serializers/inputRemoveProduct'
+import { RemoveProductOperator } from '../../3-controller/operators/removeProductOperator'
+
+export const handler = httpHandler(async (event: APIGatewayProxyEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  const operator = container.get(RemoveProductOperator)
+  const body = event.pathParameters
+
+  const input = new InputRemoveProduct(body as Object)
+  const result = await operator.exec(input)
+
+  if (result.isLeft()) {
+    return httpResponse.badRequest(result.value)
+  }
+
+  return httpResponse.ok(result.value)
+})
