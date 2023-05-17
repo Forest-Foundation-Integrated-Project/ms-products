7225feae0746b640f2e6dcd0ea5d9dc0c8ae99f2
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-17 16:01:38 -0300

    feat: add remove product use case

---
+++ src/2-business/useCases/removeProductUseCase.ts
@@ -0,0 +1,24 @@
+import { injectable, inject } from 'inversify'
+
+import { left, right } from '../../4-framework/shared/either'
+import { IUseCase } from './iUseCase'
+import { IProductRepository, IProductRepositoryToken } from '../repositories/iProductRepository'
+import { InputRemoveProductDto, OutputRemoveProductDto } from '../dto/productDto'
+import { ProductNotFound, ProductRemovalFailed } from '../module/errors/products'
+
+@injectable()
+export class RemoveProductUseCase implements IUseCase<InputRemoveProductDto, OutputRemoveProductDto> {
+  public constructor(@inject(IProductRepositoryToken) private productRepository: IProductRepository) {}
+
+  async exec(input: InputRemoveProductDto): Promise<OutputRemoveProductDto> {
+    try {
+      const productResult = await this.productRepository.remove(input.product_id)
+      
+      if (!productResult) return left (ProductNotFound)
+      
+      return right(productResult)
+    } catch (error) {
+      return left(ProductRemovalFailed)
+    }
+  }
+}
