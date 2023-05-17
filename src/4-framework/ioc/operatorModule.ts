ecccaf22a7700f7619d1f07d142c3ad13b7c1cd0
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-17 16:03:42 -0300

    feat: add remove product lambda binds

--- src/4-framework/ioc/operatorModule.ts
+++ src/4-framework/ioc/operatorModule.ts
@@ -1,8 +1,10 @@
 import { ContainerModule, interfaces } from 'inversify'
 import { CreateProductOperator } from '../../3-controller/operators/createProductOperator'
 import { ViewProductOperator } from '../../3-controller/operators/viewProductOperator'
+import { RemoveProductOperator } from '../../3-controller/operators/removeProductOperator'
 
 export const OperatorModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateProductOperator).toSelf()
   bind(ViewProductOperator).toSelf()
+  bind(RemoveProductOperator).toSelf()
 })
--- src/4-framework/ioc/useCasesModule.ts
+++ src/4-framework/ioc/useCasesModule.ts
@@ -1,8 +1,10 @@
 import { ContainerModule, interfaces } from 'inversify'
 import { CreateProductUseCase } from '../../2-business/useCases/createProductUseCase'
 import { ViewProductUseCase } from '../../2-business/useCases/viewProductUseCase'
+import { RemoveProductUseCase } from '../../2-business/useCases/removeProductUseCase'
 
 export const UseCasesModule = new ContainerModule((bind: interfaces.Bind) => {
   bind(CreateProductUseCase).toSelf()
   bind(ViewProductUseCase).toSelf()
+  bind(RemoveProductUseCase).toSelf()
 })
