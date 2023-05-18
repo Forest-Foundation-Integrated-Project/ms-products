b5218cf3e6eaca91ff3b099b17fdeecf0669836a
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-18 13:57:54 -0300

    feat: add update product lambda

---
+++ src/4-framework/functions/updateProduct.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata'
+import '../ioc/inversify.config'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+
+import { httpHandler } from '../utility/httpHandler'
+import { container } from '../shared/ioc/container'
+import { httpResponse } from '../utility/httpResponse'
+import { InputUpdateProduct } from '../../3-controller/serializers/inputUpdateProduct'
+import { UpdateProductOperator } from '../../3-controller/operators/updateProductOperator'
+
+export const handler = httpHandler(async (event: APIGatewayProxyEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  const operator = container.get(UpdateProductOperator)
+  const path = event.pathParameters
+  const body = JSON.parse(event?.body as string)
+
+  const payload = {
+    ...body,
+    product_id: path?.product_id
+  }
+
+  const input = new InputUpdateProduct(payload)
+  const result = await operator.exec(input)
+
+  if (result.isLeft()) {
+    return httpResponse.badRequest(result.value)
+  }
+
+  return httpResponse.created(result.value)
+})
